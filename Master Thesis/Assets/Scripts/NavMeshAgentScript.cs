80becfb743ae0f40cb9901494e9470f2d9bfc4aa
TMargaret <tmargaret@hotmail.com>
2016-10-19 00:47:52 +0300

    add project

---
+++ Master Thesis/Assets/Scripts/NavMeshAgentScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshAgentScript : MonoBehaviour {
+
+    public Transform goal;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+}
