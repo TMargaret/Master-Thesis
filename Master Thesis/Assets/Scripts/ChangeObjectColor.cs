4a29c6a66901c4e4bef071c716de2e1fffc91cd6
TMargaret <tmargaret@hotmail.com>
2016-10-31 01:45:29 +0200

    changes

---
+++ Master Thesis/Assets/Scripts/ChangeObjectColor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeObjectColor : MonoBehaviour {
+
+    public GUISkin GameSkin;
+    private Color startColor;
+    
+    void OnGUI()
+    {
+        GUI.skin = GameSkin;
+    }
+
+    void OnMouseEnter()
+    {
+        startColor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = startColor;
+    }
+}
