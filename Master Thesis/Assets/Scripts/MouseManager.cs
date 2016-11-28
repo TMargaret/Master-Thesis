8986a4bab57a607e6eb444d2376a2d0ecc1805f2
TMargaret <tmargaret@hotmail.com>
2016-11-28 18:01:44 +0200

    MouseManager

---
+++ Master Thesis/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour {
+
+	public GameObject selectedObject;
+	// Use this for initialization
+	void Start () {
+	
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit hitInfo;
+
+		if (Physics.Raycast (ray, out hitInfo)) {
+
+			Debug.Log ("Mouse is over:" + hitInfo.collider.name);
+			GameObject hitObject = hitInfo.transform.root.gameObject;
+
+			SelectObject (hitObject);
+		} else {
+			ClearSelection ();
+		}
+
+			
+	}
+
+	void SelectObject(GameObject obj){
+		if (selectedObject != null) {
+			if (obj == selectedObject)
+				return;
+
+			ClearSelection ();
+		}
+		selectedObject = obj;
+	
+	}
+
+	void ClearSelection(){
+		selectedObject = null;
+	}
+
+}
